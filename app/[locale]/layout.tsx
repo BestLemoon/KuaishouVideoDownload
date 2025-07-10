f638b056ec55cc869bf4488fd63f7dd82a591c34
Chillway <53417050+BestLemoon@users.noreply.github.com>
2025-07-10 09:32:30 +0800

    chore: update .gitignore to exclude seo_blog_generator and marketing_scripts directories; add Microsoft Clarity Analytics script to layout for enhanced user tracking

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -127,6 +127,20 @@ export default async function RootLayout({
           async
           strategy="afterInteractive"
         />
+        {/* Microsoft Clarity Analytics */}
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "scgux5faow");
+          `
+        }}
+      />
       </head>
       <body
         className={cn(
